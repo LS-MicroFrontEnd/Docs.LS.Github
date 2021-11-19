3d0027f5cecbf7bb0a324c96ea62966b5855642f
Carlos Lopez Soto <carlitoslopezs@hotmail.com>
2021-11-19 10:14:09 -0300

    initial commit

---
+++ LoadObjects/LoadHttpClient.cs
@@ -0,0 +1,39 @@
+﻿using RequestApi.Services.Authentication;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RequestApi.LoadObjects
+{
+    public class LoadHttpClient
+    {
+        public static async Task<HttpClient> GetHttpClientWithToken(string urlService, string username=null, string password=null,string token=null)
+        {
+            //Create and Load Http Client
+            HttpClient client = GetHttpClient(urlService: urlService);
+            //Get Token
+            if(string.IsNullOrEmpty(token) && !string.IsNullOrEmpty(username) && !string.IsNullOrEmpty(password))
+            {
+                AuthenticationRequest authenticationRequest = new AuthenticationRequest(client);
+                token = await authenticationRequest.GetToken(username, password);
+            }
+            //Load Token to Http header
+            if(!string.IsNullOrEmpty(token))
+            {
+                client.DefaultRequestHeaders.Add("Authorization", "Bearer " + token);
+            }
+            return client;
+        }
+        public static HttpClient GetHttpClient(string urlService, int TimeOut = 50000)
+        {
+            //Create and Load Http Client
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(urlService);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.Timeout = TimeSpan.FromSeconds(TimeOut);
+            return client;
+        }
+    }
+}
---
+++ Services/Authentication/AuthenticationRequest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using RequestApi.LoadObjects;
+
+namespace RequestApi.Services.Authentication
+{
+    public class AuthenticationRequest
+    {
+        readonly HttpClient _client;
+
+        //Instancio http client en el constructor
+        public AuthenticationRequest(HttpClient client)
+        {
+            _client = client;
+        }
+        public AuthenticationRequest(string urlService)
+        {
+            _client = LoadHttpClient.GetHttpClient(urlService);
+        }
+
+        public async Task<string> GetToken(string username, string password, string endPoint = "api/account/login")
+        {
+            try
+            {
+                //Creo un objeto usuario con el usuario u contraseña
+                var user = new { username, password };
+                //serializo el usuario en un string
+                string userSerilize = JsonSerializer.Serialize(user);
+                //agrego el usuario en el http content
+                HttpContent httpContent = new StringContent(userSerilize, Encoding.UTF8, "application/json");
+                //Realizo el post
+                HttpResponseMessage response = await _client.PostAsync(endPoint, httpContent);
+                //Leo el resultado
+                string jsonResponseResult = response.Content.ReadAsStringAsync().Result;
+                //Obtengo el token
+                JsonDocument document = JsonDocument.Parse(jsonResponseResult);
+                JsonElement root = document.RootElement;
+                JsonElement tokenElement = root.GetProperty("token");
+                //Devuelvo el token
+                return tokenElement.GetString() ;
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
